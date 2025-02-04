62524d16a0cfc8d130f395af76e535fc5e16429b
Gary Mawdsley <79173+gmawdo@users.noreply.github.com>
2025-02-04 22:38:55 +0000

    more helpful info

--- mount_nfs.sh
+++ mount_nfs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Mount the NFS share
+# Note dolphin is the hard coded NFS username and the NFS parses the name from the string "dolphin's drive"
 mount -t nfs -o nolock,tcp,rsize=131072,actimeo=120,port=2049,mountport=2049 host.docker.internal:"/dolphin's drive" /mnt/gray
